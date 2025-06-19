6bd20f8f5d71403be3413bb7aa534faaf24e2c70
神戸龍谷図書委員会 <koberyukoku.library.staff@gmail.com>
2025-06-19 16:18:34 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCN0xbBPlPJM1szoOik6yMFgkiNRy_QhZY",
   authDomain: "koberyukoku-library-staff.firebaseapp.com",
   projectId: "koberyukoku-library-staff",
-  storageBucket: "koberyukoku-library-staff.firebasestorage.app",
+  storageBucket: "koberyukoku-library-staff.appspot.com",
   messagingSenderId: "138126502371",
   appId: "1:138126502371:web:c613b19bba31ef3c87d8d6",
   measurementId: "G-P6N0TFRY9Q"
